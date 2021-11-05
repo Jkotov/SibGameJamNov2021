8bd8f6cd0488a204ac66bf32ce5543508f57d921
epainter <gavgavov@gmail.com>
2021-11-05 18:14:45 +0300

    bricks

--- Assets/Brick.cs
+++ Assets/Brick.cs
@@ -1,18 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float rotationSpeed;
+    private Camera _camera;
+    private bool _followingMouse;
+    private Vector3 _prevMousePos;
+    private Rigidbody2D _rigidbody2D;
+    private Collider2D _collider2D;
+    private Vector3 _defaultPos;
+    private SpriteRenderer _renderer;
+
+    private void Awake()
     {
+        _camera = Camera.main;
+        tag = "Brick";
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _collider2D = GetComponent<Collider2D>();
+        _defaultPos = transform.position;
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (_followingMouse)
+        {
+            var deltaMouse = _camera.ScreenToWorldPoint(Input.mousePosition) - _prevMousePos;
+            transform.position += deltaMouse;
+            _prevMousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+            if (Input.GetKey(KeyCode.R))
+            {
+                transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+            }
+        }
+
+        if (!_followingMouse && !_renderer.isVisible)
+        {
+            transform.position = _defaultPos;
+        }
+    }
+    
+    
+
+    private void OnMouseDown()
+    {
+        _followingMouse = true;
+        _prevMousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+        _rigidbody2D.bodyType = RigidbodyType2D.Static;
+        _collider2D.isTrigger = true;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseUp()
     {
-        
+        _followingMouse = false;
+        _collider2D.isTrigger = false;
+        _rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
     }
+
 }
