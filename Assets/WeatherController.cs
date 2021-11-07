274cb0d9ab0d3b02f23269b6630a190aa9db26eb
epainter <gavgavov@gmail.com>
2021-11-07 15:37:08 +0300

    anims

--- Assets/WeatherController.cs
+++ Assets/WeatherController.cs
@@ -9,6 +9,8 @@ public class WeatherController : MonoBehaviour
     [SerializeField] private float firstEarthTimeout;
     [SerializeField] private float timeBetweenEarth;
     [SerializeField] private float timeBetweenWindLighting;
+    [SerializeField] private GameObject tree;
+    private Animator _treeAnimator;
     private Controller _fan;
     private Controller _lighting;
     private Controller _earth;
@@ -17,6 +19,7 @@ public class WeatherController : MonoBehaviour
     {
         _fan = GameObject.Find("Fans").GetComponent<Controller>();
         _earth = GameObject.Find("EarthController").GetComponent<Controller>();
+        _treeAnimator = tree.GetComponent<Animator>();
         StartCoroutine(StartFirstWind());
         StartCoroutine(StartFirstEarth());
     }
@@ -45,11 +48,20 @@ public class WeatherController : MonoBehaviour
     private IEnumerator WindAndLight()
     {
         _fan.ActiveCrisis();
+        StartCoroutine(TreeAnim());
         while (true)
         {
             yield return new WaitForSeconds(timeBetweenWindLighting + _fan.Timeout);
             _fan.ActiveCrisis();
+            StartCoroutine(TreeAnim());
         }
+    }
+
+    private IEnumerator TreeAnim()
+    {
+        _treeAnimator.SetBool("WindActive", true);
+        yield return new WaitForSeconds(_fan.Timeout);
+        _treeAnimator.SetBool("WindActive", false);
     }
     
     
