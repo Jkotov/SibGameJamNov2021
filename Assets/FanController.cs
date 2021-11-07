02a3bc6f03750f6682d7fb3a07cff8040216e4a3
epainter <gavgavov@gmail.com>
2021-11-07 03:38:47 +0300

    Warning Sign

--- Assets/FanController.cs
+++ Assets/FanController.cs
@@ -7,19 +7,32 @@ using Random = UnityEngine.Random;
 public class FanController : Controller
 {
     private ParticleSystem[] _fans;
-
+    private SpriteRenderer[] _warnings;
+    private int _index;
     private void Awake()
     {
         _fans = GetComponentsInChildren<ParticleSystem>();
+        _warnings = new SpriteRenderer[_fans.Length];
+        for (int i = 0; i < _fans.Length; i++)
+        {
+            _warnings[i] = _fans[i].gameObject.GetComponentInChildren<SpriteRenderer>();
+            _warnings[i].enabled = false;
+        }
     }
 
+    private protected override IEnumerator ShowWarning()
+    {
+        _index = Random.Range(0, _fans.Length);
+        _warnings[_index].enabled = true;
+        yield return base.ShowWarning();
+        _warnings[_index].enabled = false;
+    }
 
     private protected override IEnumerator Crisis()
     {
         Debug.Log("here");
-        var index = Random.Range(0, _fans.Length);
-        _fans[index].Play();
+        _fans[_index].Play();
         yield return new WaitForSeconds(timeout);
-        _fans[index].Stop();
+        _fans[_index].Stop();
     }
 }
